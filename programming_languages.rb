0135e571222552944ae5db7f13ced189eab992b8
Paul Clark <clark.paul123@gmail.com>
2018-10-23 22:21:40 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  languages_reformatted = {}
+  languages.each do |style, language|
+    language.each do |name, type|
+      languages_reformatted[name] = type
+      languages_reformatted[name][:style] = []
+      languages_reformatted[name][:style] << style
+     end
+  end
+  languages_reformatted[:javascript][:style] << :oo
+  languages_reformatted
 end
+
+#there must be a better way to add :oo to the javascript array
